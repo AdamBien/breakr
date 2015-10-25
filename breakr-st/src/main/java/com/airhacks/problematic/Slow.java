f6e7201dd6227d1c8e485c11cf2d78b0cb7755c3
Adam Bien <abien@adam-bien.com>
2015-10-25 02:03:43 -0700

    Dependency to released version added

--- breakr-st/src/main/java/com/airhacks/problematic/Slow.java
+++ breakr-st/src/main/java/com/airhacks/problematic/Slow.java
@@ -2,8 +2,6 @@ package com.airhacks.problematic;
 
 import com.airhacks.breakr.Breakr;
 import com.airhacks.breakr.IgnoreCallsWhen;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.ejb.Singleton;
 import javax.interceptor.Interceptors;
 
@@ -20,7 +18,6 @@ public class Slow {
         try {
             Thread.sleep(100);
         } catch (InterruptedException ex) {
-            Logger.getLogger(Slow.class.getName()).log(Level.SEVERE, null, ex);
         }
         return "Slow: " + System.currentTimeMillis();
     }
