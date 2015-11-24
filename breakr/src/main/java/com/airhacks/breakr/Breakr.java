bfac128d786f0461eacf0336697484c0a4bdb99b
Adam Bien <abien@adam-bien.com>
2015-11-24 21:54:11 +0100

    Circuit management delegated to a configurable class

--- breakr/src/main/java/com/airhacks/breakr/Breakr.java
+++ breakr/src/main/java/com/airhacks/breakr/Breakr.java
@@ -9,9 +9,9 @@ package com.airhacks.breakr;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -19,7 +19,8 @@ package com.airhacks.breakr;
  * limitations under the License.
  * #L%
  */
-
+import java.lang.reflect.Method;
+import javax.inject.Inject;
 import javax.interceptor.AroundInvoke;
 import javax.interceptor.InvocationContext;
 
@@ -29,37 +30,38 @@ import javax.interceptor.InvocationContext;
  */
 public class Breakr {
 
-    private long errorCounter;
-    private static final int FAILURES_COUNT = 5;
-    private static final int TIMEOUT_IN_MS = 1000;
+    private static final long FAILURES_COUNT = 5;
+    private static final long TIMEOUT_IN_MS = 1000;
+
+    @Inject
+    Circuit circuit;
 
     @AroundInvoke
     public Object guard(InvocationContext ic) throws Exception {
-        long maxNbrOfFailures = FAILURES_COUNT;
-        long timeout = TIMEOUT_IN_MS;
-
-        IgnoreCallsWhen configuration = ic.
-                getMethod().
+        long maxFailures = FAILURES_COUNT;
+        long maxDuration = TIMEOUT_IN_MS;
+        Method method = ic.getMethod();
+        IgnoreCallsWhen configuration = method.
                 getAnnotation(IgnoreCallsWhen.class);
-
         if (configuration != null) {
-            maxNbrOfFailures = configuration.failures();
-            timeout = configuration.slowerThanMillis();
+            maxFailures = configuration.failures();
+            maxDuration = configuration.slowerThanMillis();
         }
 
         long start = System.currentTimeMillis();
         try {
-            if (errorCounter >= maxNbrOfFailures) {
+            if (circuit.isOpen(maxFailures)) {
                 return null;
             }
             return ic.proceed();
         } catch (Exception ex) {
-            errorCounter++;
+            circuit.newException(ex);
             throw ex;
         } finally {
             long duration = System.currentTimeMillis() - start;
-            if (duration >= timeout) {
-                errorCounter++;
+
+            if (duration > maxDuration) {
+                this.circuit.newTimeout(duration);
             }
         }
     }
