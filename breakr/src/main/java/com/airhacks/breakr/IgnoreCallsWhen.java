5071c0fcccded8ff6b4c37fcb65c6914e44eaf42
Adam Bien <abien@adam-bien.com>
2015-11-25 14:10:53 +0100

    JavaDoc added

--- breakr/src/main/java/com/airhacks/breakr/IgnoreCallsWhen.java
+++ breakr/src/main/java/com/airhacks/breakr/IgnoreCallsWhen.java
@@ -9,9 +9,9 @@ package com.airhacks.breakr;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -19,7 +19,6 @@ package com.airhacks.breakr;
  * limitations under the License.
  * #L%
  */
-
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -27,13 +26,26 @@ import java.lang.annotation.Target;
 
 /**
  *
+ * This optional annotation can be used to individually override the default
+ * configuration of the circuit breaker.
+ *
  * @author airhacks.com
  */
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface IgnoreCallsWhen {
 
-    long slowerThanMillis() default 1000;
+    public static final long TIMEOUT = 1000;
+    public static final long MAX_FAILURES = 3;
 
-    long failures() default 3;
+    /**
+     * 1s max execution time of any public method of the monitored class.
+     */
+    long slowerThanMillis() default TIMEOUT;
+
+    /**
+     * The max number of failures (exception occurrences and timeouts) before
+     * the circuit opens.
+     */
+    long failures() default MAX_FAILURES;
 }
