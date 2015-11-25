418b7a92b77c04440fe649c81fadde985f5e90cb
Adam Bien <abien@adam-bien.com>
2015-11-25 13:56:06 +0100

    JavaDoc added

--- breakr-st/src/main/java/com/airhacks/problematic/Brittle.java
+++ breakr-st/src/main/java/com/airhacks/problematic/Brittle.java
@@ -13,10 +13,18 @@ import javax.interceptor.Interceptors;
  * @author airhacks.com
  */
 @Singleton
+/**
+ * Breakr is implemented as an Java EE interceptor. Annotate a
+ * <code>@Singleton</code> CDI or EJB to activate the call supervision.
+ */
 @Interceptors(Breakr.class)
 @ConcurrencyManagement(ConcurrencyManagementType.BEAN)
 public class Brittle {
 
+    /**
+     * With the optional <code>@IgnoreCallsWhen</code> you can override the
+     * (timeout = 1s, maxFailures = 5).
+     */
     @IgnoreCallsWhen(failures = 2)
     public String test(boolean exception) throws Exception {
         if (exception) {
@@ -25,6 +33,10 @@ public class Brittle {
         return "works " + System.currentTimeMillis();
     }
 
+    /**
+     * Calling a method annotated with <code>@CloseCircuit</code> resets all
+     * counters and closes the circuit.
+     */
     @CloseCircuit
     public void reset() {
     }
