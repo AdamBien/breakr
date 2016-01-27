1e70644793b61f055c0d9cd246d9072739d2a87e
Adam Bien <abien@adam-bien.com>
2016-01-27 10:06:01 +0100

    AtomicBoolean introduced

--- breakr/src/test/java/com/airhacks/breakr/CustomCircuit.java
+++ breakr/src/test/java/com/airhacks/breakr/CustomCircuit.java
@@ -24,9 +24,9 @@ package com.airhacks.breakr;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -34,6 +34,7 @@ package com.airhacks.breakr;
  * limitations under the License.
  * #L%
  */
+import java.util.concurrent.atomic.AtomicBoolean;
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.inject.Specializes;
 
@@ -45,19 +46,21 @@ import javax.enterprise.inject.Specializes;
 @Specializes
 public class CustomCircuit extends Circuit {
 
-    private boolean open;
+    private AtomicBoolean open = new AtomicBoolean(false);
 
     @Override
     public boolean isOpen(long maxFailures) {
-        return open;
+        return open.get();
     }
 
     public long getFailureCount() {
         return failureCounter.get();
     }
 
+    /**
+     * controls the closing and opening of the circuit
+     */
     public void setOpen(boolean open) {
-        this.open = open;
+        this.open.set(open);
     }
-
 }
